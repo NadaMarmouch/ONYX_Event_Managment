7f193a981c883afa9ddbafa8fa78a1c274930e49
mariiamelbeheiry <mariam1806659@miuegypt.edu.eg>
2021-06-23 23:06:36 +0200

    Controller

---
+++ Event_mangement_system/app/controller/EventsController.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once( "..\app\controller/Controller.php");
+include "..\app\model\Model.php";
+
+class EventsController extends Controller{
+	public function insert() {
+		$EventView= new EventView($this,$this->model);
+		$EventModel= new Events();
+		
+		if(isset($_POST['submit']))
+		{	
+			$EventModel->setEventname($_POST['type']);
+			$EventModel->setStartdate($_POST['Start_Date']);
+			$EventModel->setEnddate($_POST['End_Date']);
+			$EventModel->setNumofpart($_POST['Guests']);
+			$EventModel->setEventtype($_POST['type']);
+			$EventModel->setVenuename($_POST['venue']);
+			$EventModel->setEventstatus($_POST['status']);
+			$EventModel->setComments($_POST['comments']);
+			$EventModel->insertEvent();
+		}
+		
+	}
+
+	// public function edit($movie_id) {
+	// 	$name = $_REQUEST['name'];
+	// 	$year = $_REQUEST['year'];
+
+	// 	$this->model->getMovie($movie_id)->editMovie($name,$year);
+	// }
+
+	// public function delete($movie_id){
+	//	$this->model->getMovie($movie_id)->deleteMovie();
+	// }
+}
+?>
---
+++ Event_mangement_system/public/control.php
@@ -0,0 +1,10 @@
+<?php
+include '../app/view/ViewEvent.php';
+include '../app/controller/EventsController.php';
+include '../app/model/Events.php';
+$model = new Events();
+$controller = new EventsController($model);
+$view = new EventView($controller, $model);
+echo $view->output();
+$controller->insert();
+?>
