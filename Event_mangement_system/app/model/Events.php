3de331c9b4c02ba755b2e2c5530c0c74fbf61f76
mariiamelbeheiry <mariam1806659@miuegypt.edu.eg>
2021-06-24 05:09:48 +0200

    update

--- Event_mangement_system/app/model/Events.php
+++ Event_mangement_system/app/model/Events.php
@@ -72,14 +72,13 @@ $this->comments=" ";
 
     
     function insertEvent(){
-
-		$sql = "INSERT INTO `events`(`user_id`, `event_name`, `start_date`, `end_date`, `num_of_part`, `event_type`, `venue_name`, `event_status`, `comments`) 
-        VALUES ('$_SESSION[id]','$this->event_name','$this->start_date','$this->end_date','$this->num_of_part','$this->event_type','$this->venue_name','$this->event_status','$this->comments')";
+        echo"jehhererrehfrfr";
+		$sql = "INSERT INTO `events`(`user__id`, `event_name`, `start_date`, `end_date`, `num_of_part`, `event_type`, `venue_name`, `event_status`, `comments`) 
+        VALUES ('1','$this->event_name','$this->start_date','$this->end_date','$this->num_of_part','$this->event_type','$this->venue_name','$this->event_status','$this->comments')";
         $db=new DBH();
         mysqli_query($db->connectDB(),$sql);
-}
-
-
+       
+    }
 public function view_Events_Types(){
     $dbvariable=new DBH();
 
